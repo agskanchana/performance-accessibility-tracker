d6e8cb7d2fd1e522afc407d2b7d4e80b93c5ad89
agskanchana <sameerakanchana@live.com>
2025-07-20 21:37:33 +0530

    fixed issues config

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuration file for PageSpeed Analyzer
+// Keep this file secure and don't share it publicly
+
+const CONFIG = {
+    PAGESPEED_API_KEY: 'AIzaSyC4q_ND3Fdt2XdTCNsdyZi7fckGAqEwxD4'
+};
+
+// Export for use in the main application
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
